165630201af9e8294864eb5ab504ce78df5d9732
UltiUltimat3 <113640967+UltiUltimat3@users.noreply.github.com>
2022-12-11 10:52:18 -0300

    Add files via upload

--- js/PlayerArrow.js
+++ js/PlayerArrow.js
@@ -34,18 +34,20 @@ class PlayerArrow {
 
       if (this.body.velocity.x > 0 && this.body.position.x > 400) {
         var position = [this.body.position.x, this.body.position.y];
-        /****escreva um 
-         código para adicionar a posição 
-         atual da flecha a 
-        matriz trajectory (trajetória)**/
-      
-      }
-  
+    
 
-      /****escreva um código correto para adicionar o loop for e exibir pequenos pontos
-           em todas as posições armazenadas na matriz trajectory (trajetória)
-           *******/
-     
-  
-  }
+       if (this.body.velocity.x > 0 && this.body.position.x > 400) {
+      var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position);
+
+
+        for (var i = 0; i < this.trajectory.length; i++) {
+          fill("white");
+          ellipse(this.trajectory[i][0], this.trajectory[i][1], 5, 5);
+        }
+
+      }
+
+}
+}
 }
